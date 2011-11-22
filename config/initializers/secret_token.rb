7b21085d09e3d423b047dc6997dc52610f11a607
Glenn <gleenn@gmail.com>
2011-11-22 00:07:33 -0800

    initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LinearRegression::Application.config.secret_token = '28ccb2386f589f73b81dbdcb66fc8e1544835760914cb4d1f44681be0381533466485e7e3f46d981411b752bbce3498297a19c3464694ea116ed5259a908dc78'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+LinearRegression::Application.config.session_store :cookie_store, key: '_linear_regression_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# LinearRegression::Application.config.session_store :active_record_store
